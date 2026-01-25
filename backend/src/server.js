2c69698922136c55250b9de1bfdf3d4f9c207629
Shivam Roy <shivamroy344785@gmail.com>
2026-01-26 02:32:53 +0530

    database set

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express()
 
@@ -9,5 +10,22 @@ console.log(ENV.DB_URL);
 app.get("/health",(req,res) => {
     res.status(200).json({msg:"api is up and running"});
 });
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT    ));
+app.get("/{*any}",(req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
+
+
+
+    const startServer = async () => {
+        try{
+            await connectDB();
+        app.listen(ENV.PORT, () =>console.log("Server is running on port:", ENV.PORT)); 
+        }catch(error){
+         console.error("💥Error starting the server",error)
+        }
+    };
+    startServer(); 

0cc48ddd92fb4f8a84d18a3f5db8f274e1c278ac
Shivam Roy <shivamroy344785@gmail.com>
2026-03-22 22:46:01 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,35 @@
-import express from "express"
-import path from "path"
+import express from "express";
+import path from "path";
+import cors from "cors";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-
+import { serve } from "inngest/express"
+import { inngest,functions } from "./lib/inngest.js";
 const app = express()
 
 const __dirname = path.resolve()
+
+//middleware
+app.use(express.json())
+//credentials:true meaning?? => server allows a browser to include cokkies on request
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client: inngest, functions}));
+
 app.get("/health",(req,res) => {
     res.status(200).json({msg:"api is up and running"});
-});
+}); 
 app.get("/books",(req,res) => {
     res.status(200).json({msg:"this  is the books endpoint"});
 });
 
 if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("/{*any}",(req,res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 }
-
 
 
     const startServer = async () => {

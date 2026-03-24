ff0ed54b6de9c39d53c2aa50d65b52508940a826
Shivam Roy <shivamroy344785@gmail.com>
2026-03-24 14:26:04 +0530

    auth & stream sync

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -2,13 +2,13 @@ import {StreamChat} from "stream-chat"
 import { ENV } from "./env.js"
 
 const apiKey = ENV.STREAM_API_KEY
-const apiSecret = ENV.STREAM_API_SECERET
+const apiSecret = ENV.STREAM_API_SECRET
 
 if(!apiKey || !apiSecret){
-    console.error("STREAM_API_KEY or STREAM_API_SECERET is missing");
+    console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
-export const chartClient = StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
 
 
 export const upsertStreamUser = async(userData) => {

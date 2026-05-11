ceb99c71634118a2f39d7319a74fd243c91d6930
Shivam Roy <shivamroy344785@gmail.com>
2026-05-11 12:44:24 +0530

    fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import path from "path";
 import cors from "cors";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express"
 import { inngest,functions } from "./lib/inngest.js";
 const app = express()
@@ -13,14 +14,21 @@ const __dirname = path.resolve()
 app.use(express.json())
 //credentials:true meaning?? => server allows a browser to include cokkies on request
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use(clerkMiddleware()); //this adds auth field to request object: req.auth()
 
 app.use("/api/inngest",serve({client: inngest, functions}));
 
 app.get("/health",(req,res) => {
+    req.auth
     res.status(200).json({msg:"api is up and running"});
 }); 
 app.get("/books",(req,res) => {
     res.status(200).json({msg:"this  is the books endpoint"});
+});
+//when u pass an array of middleware to express,it automatically flattens and executes 
+//them sequentially,one by one
+app.get("/video-calls", protecRoute,(req,res) => {
+    res.status(200).json({msg:"this  is a protected route"});
 });
 
 if(ENV.NODE_ENV === "production"){
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,17 @@
 import { SignedOut, SignedIn, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
 import toast from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
 
 function HomePage() {
   
-  const {data, isLoading, error} = useQuery({
-    queryFn: () => fetch("/api/books").then(res => res.json())
-  })
+  // const {data, isLoading, error} = useQuery({
+  //   queryFn: () => fetch("/api/books").then(res => res.json())
+  // })
 
     return (
       <div>
-        <button className="btn btn-secondary" onClick={() => toast.success("This is success")}>Click me</button>
+        <button className="btn btn-secondary" onClick={() => 
+          toast.error("This is a success toast")}>Click me</button>
 
         <SignedOut>
           <SignInButton mode="modal">
